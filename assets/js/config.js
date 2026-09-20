785d6078e33c8b3c08fd4d442fe2e8ec5bc7ea61
fernandolmarcos00-ops <fernandol.marcos00@gmail.com>
2026-09-20 10:01:56 -0300

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,5 @@
+// Cole aqui a URL e a chave PÚBLICA (anon) do Supabase.
+// NUNCA use a service_role key no navegador.
+window.SUPABASE_URL = 'https://njhvharlupxuhxzbgftv.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_IKywo7CZ7K5-Fl2yulOa_A_14CA8t2f';
+window.SUPABASE_BUCKET = 'product-images';
